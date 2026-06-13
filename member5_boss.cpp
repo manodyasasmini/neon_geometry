ae7fde8487088c1ce8d49b87ecf6295f9d48ef8e
Sharada Abeywickrama <sharadajayashana@gmail.com>
2026-06-13 16:35:22 +0530

    CUSTOM SHEAR MATRIX,PRIMITIVE: SQUARE

--- member5_boss.cpp
+++ member5_boss.cpp
@@ -10,6 +10,27 @@ namespace Member5_Boss {
     float scaleX = 1.0f, scaleY = 1.0f;
     float spikes[8] = {1, 1, 1, 1, 1, 1, 1, 1};
 
+ // CUSTOM SHEAR MATRIX
+    void applyShear(float shx, float shy) {
+        float m[16] = {
+            1.0f, shy,  0.0f, 0.0f,   // column 1
+            shx,  1.0f, 0.0f, 0.0f,   // column 2
+            0.0f, 0.0f, 1.0f, 0.0f,   // column 3
+            0.0f, 0.0f, 0.0f, 1.0f    // column 4
+        };
+        glMultMatrixf(m);
+    }
+
+    // PRIMITIVE: SQUARE
+    void drawSquare(float s, float r, float g, float b) {
+        glColor3f(r, g, b);
+        glBegin(GL_QUADS);
+            glVertex2f(-s, -s);
+            glVertex2f( s, -s);
+            glVertex2f( s,  s);
+            glVertex2f(-s,  s);
+        glEnd();
+    }
 
 
     }
